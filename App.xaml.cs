8a96573b73dd9019b1f155c2da6cae3bea84dd05
Phat Tran <ptran@strongtie.com>
2024-03-29 19:33:58 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Vitalize_Vault
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
