4ab8665f6b50b4bdfee2e6871b6770f837ca8aca
Phat Tran <ptran@strongtie.com>
2024-03-31 13:12:47 +0700

    Update ProductItemViewModel

--- ViewModel/ProductItemViewModel.cs
+++ ViewModel/ProductItemViewModel.cs
@@ -1,7 +1,36 @@
-﻿namespace Vitalize_Vault.ViewModel
+﻿using Vitalize_Vault.Model;
+
+namespace Vitalize_Vault.ViewModel
 {
     public class ProductItemViewModel : ViewModelBase
     {
+        private readonly Product _product;
 
+        public ProductItemViewModel(Product product)
+        {
+            _product = product;
+        }
+
+        public int Id => _product.Id;
+
+        public string Name
+        {
+            get => _product.Name;
+            set 
+            { 
+                _product.Name = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DateTime? ExprirationDate
+        {
+            get => _product.ExpirationDate;
+            set
+            {
+                _product.ExpirationDate = value;
+                RaisePropertyChanged();
+            }
+        }
     }
 }
