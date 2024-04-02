8c45352b5a8fa7545ae18d2f8007eb270cb4a810
Phat Tran <ptran@strongtie.com>
2024-04-03 05:42:17 +0700

    Visibility to ListView

---
+++ Converter/VisibilityToProductsListViewConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using Vitalize_Vault.ViewModel;
+
+namespace Vitalize_Vault.Converter
+{
+    public class VisibilityToProductsListViewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var products = value as List<ProductItemViewModel>;
+            if (products != null)
+            {
+                if (!products.Any()) 
+                {
+                    return Visibility.Hidden;
+                }
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
