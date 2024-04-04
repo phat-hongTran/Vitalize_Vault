1c81e891dbc8fadcf790eda42a2e5377a33ad505
Phat Tran <ptran@strongtie.com>
2024-04-04 07:09:26 +0700

    Add visual cues to highlight products nearing expiration dates

---
+++ Converter/ExpirationDateToBackgroundColorConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Vitalize_Vault.Converter
+{
+    public class ExpirationDateToBackgroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return Brushes.LightGray;
+
+            DateTime date = (DateTime)value;
+
+            TimeSpan difference = DateTime.Now - date;
+
+            double days = difference.TotalDays;
+
+            if (days < 0) 
+            {
+                return null;
+            };
+
+            switch ((int)days)
+            {
+                case 0:
+                    return Brushes.Yellow;
+                case >= 1:
+                    return Brushes.Orange;
+                default:
+                    return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
